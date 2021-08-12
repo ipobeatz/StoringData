9004ee77f9d5c72e25563bef50479e12bf0a1d25
Mrtckr008 <32914909+Mrtckr008@users.noreply.github.com>
2021-08-13 02:23:58 +0300

    Storing Data

--- app/src/main/java/com/ibrahim/storingdata/MainActivity.java
+++ app/src/main/java/com/ibrahim/storingdata/MainActivity.java
@@ -5,16 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    SharedPreferences sharedPreferences;
+    private int storedData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = findViewById(R.id.editTextNumber);
+        textView = findViewById(R.id.textView);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences('com.ibrahim.storingdata', Context.MODE_PRIVATE);
-        int age = 40;
+
+        sharedPreferences = this.getSharedPreferences("com.ibrahim.storingdata", Context.MODE_PRIVATE);
+        int storedAge = sharedPreferences.getInt("storedAge",0);
+        if (storedAge == 0) {
+            textView.setText("Your age");
+
+        }else{
+            textView.setText("Your Age"+ storedAge);
+        }
+
+
     }
+
+
+    public void save(View view) {
+
+
+
+    if (!editText.getText().toString().matches("")) {
+        int userAge = Integer.parseInt(editText.getText().toString());
+        textView.setText("your age:" + userAge);
+
+        sharedPreferences.edit().putInt("StoredAge",userAge).apply();
+
+
+        }
+    }
+    public void delete(View view) {
+        int storedDate = sharedPreferences.getInt("storedAge", 0);
+
+        if (storedData != 0) {
+            sharedPreferences.edit().remove("storedAge").apply();
+            textView.setText("Your age");
+        }
+
+    }
+
+
 }
