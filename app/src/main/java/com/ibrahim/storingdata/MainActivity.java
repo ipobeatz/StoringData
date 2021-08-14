e285155746a4e8dd058bf11f28d2be2fe954ec35
Mrtckr008 <32914909+Mrtckr008@users.noreply.github.com>
2021-08-15 01:18:14 +0300

    .

--- app/src/main/java/com/ibrahim/storingdata/MainActivity.java
+++ app/src/main/java/com/ibrahim/storingdata/MainActivity.java
@@ -1,7 +1,5 @@
 package com.ibrahim.storingdata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,13 +7,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    TextView textView;
-    SharedPreferences sharedPreferences;
-    private int storedData;
+    EditText editText, editText2;
+    TextView textView, textView2;
+    SharedPreferences sharedPreferences, sharedPreferencesv2;
 
+    String storedDataKey = "StoredData";
+    String storedDataKeyv2 = "StoredDatav2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +24,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.editTextNumber);
+        editText2 = findViewById(R.id.editTextNumber2);
         textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
 
 
         sharedPreferences = this.getSharedPreferences("com.ibrahim.storingdata", Context.MODE_PRIVATE);
-        int storedAge = sharedPreferences.getInt("storedAge",0);
+        int storedAge = sharedPreferences.getInt(storedDataKey, 0);
         if (storedAge == 0) {
-            textView.setText("Your age");
+            textView.setText("Your age :");
+        } else {
+            textView.setText("Your Age :" + storedAge);
 
-        }else{
-            textView.setText("Your Age"+ storedAge);
         }
-//
+
+        sharedPreferencesv2 = this.getSharedPreferences("com.ibrahim.storingdata", Context.MODE_PRIVATE);
+        int storedAgev2 = sharedPreferencesv2.getInt(storedDataKeyv2, 0);
+        if (storedAgev2 == 0) {
+            textView2.setText("Your agev2 :");
+        } else {
+            textView2.setText(" your age v2 : " + storedAgev2);
+
+
+        }
 
     }
 
@@ -42,22 +54,34 @@ public class MainActivity extends AppCompatActivity {
     public void save(View view) {
 
 
+        if (!editText.getText().toString().matches("")) {
+            try {int userAge = Integer.parseInt(editText.getText().toString());
 
-    if (!editText.getText().toString().matches("")) {
-        int userAge = Integer.parseInt(editText.getText().toString());
-        textView.setText("your age:" + userAge);
+                int userAgev2 = Integer.parseInt(editText2.getText().toString());
 
-        sharedPreferences.edit().putInt("StoredAge",userAge).apply();
+                textView.setText("your age :" + userAge);
+                textView2.setText("your agev2 :" + +userAgev2);
+
+                sharedPreferences.edit().putInt(storedDataKey, userAge).apply();
+                sharedPreferencesv2.edit().putInt(storedDataKeyv2, userAgev2).apply();
+
+            }
+            catch (Exception e){
+
+            }
 
 
         }
     }
+
+
     public void delete(View view) {
-        int storedDate = sharedPreferences.getInt("storedAge", 0);
+        int storedData = sharedPreferences.getInt(storedDataKey, 0);
 
         if (storedData != 0) {
-            sharedPreferences.edit().remove("storedAge").apply();
-            textView.setText("Your age");
+            sharedPreferences.edit().remove(storedDataKey).apply();
+            textView.setText("your age :");
+            textView2.setText("your agev2 :");
         }
 
     }
